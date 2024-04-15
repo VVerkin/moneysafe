21259621c9a12ecb4fefdc460231742294f74db4
Vasilii Verkin <v.verkin@gmail.com>
2024-04-15 21:04:46 +0300

    открытие и закрытие отчета, подключение видоизмененных сайдбарров

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import {convertStringNumber} from './convertStringNumber.js';
+// Импортируем скроллбары
+import {OverlayScrollbars} from './overlayscrollbars.esm.min.js';
 
 // Получаем форму по классу
 const financeForm = document.querySelector('.finance__form');
@@ -6,12 +8,14 @@ const financeForm = document.querySelector('.finance__form');
 const financeAmount = document.querySelector('.finance__amount');
 // Получаем кнопку "отчет"
 const financeReport = document.querySelector('.finance__report');
+// Получаем отчет
+const report = document.querySelector('.report');
 
 let amount = 0;
 
 financeAmount.taxtContent = amount;
 
-//
+// Ф-я выполняет добавление или уменьшение суммы при нажатии на "+" или "-"
 financeForm.addEventListener('submit', (e) => {
     e.preventDefault();
     // Определяем тип операции (нажатие на "-" - expenses, на "+" - income)
@@ -29,11 +33,28 @@ financeForm.addEventListener('submit', (e) => {
     }
     // Результат выводим с разделением тысяч и добавлением знака рубля
     financeAmount.textContent = `${amount.toLocaleString()} ₽`;
-
-
 });
 
-financeReport.addEventListener('click', () => {
-const report = document.querySelector('.report');
-report.classList.add('report__open');
-});
+// Активируем скроллбары
+OverlayScrollbars(report, {});
+
+// Ф-я закрывает отчет
+// В параметрах получаем target деструктуризацией
+const closeReport = ({target}) => {
+    // окно закрывается при нажатии на крестик или вне окна.
+    if (target.closest('.report__close') || (!target.closest('.report') && target !== financeReport)) {
+    report.classList.remove('report__open');
+    }
+};
+
+// Ф-я открывает отчет
+const openReport = () => {
+    report.classList.add('report__open');
+
+document.addEventListener('click', closeReport);
+};
+
+// Навешиваем событие на клик по кнопке "отчет"
+financeReport.addEventListener('click', openReport);
+
+
