105b06b80747909674fb235ec8c641ee5ddd91ab
Vasilii Verkin <v.verkin@gmail.com>
2024-04-15 22:25:16 +0300

    подключили сервер и реализовали заполнение строк таблицы данными с сервера

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ import {convertStringNumber} from './convertStringNumber.js';
 // Импортируем скроллбары
 import {OverlayScrollbars} from './overlayscrollbars.esm.min.js';
 
+const API_URL = 'https://deluxe-meadow-trumpet.glitch.me/api'
+
 // Получаем форму по классу
 const financeForm = document.querySelector('.finance__form');
 // Получим итоговое значение
@@ -10,6 +12,8 @@ const financeAmount = document.querySelector('.finance__amount');
 const financeReport = document.querySelector('.finance__report');
 // Получаем отчет
 const report = document.querySelector('.report');
+// Получаем tbody по классу
+const reportOperationList = document.querySelector('.report__operation-list');
 
 let amount = 0;
 
@@ -37,6 +41,24 @@ financeForm.addEventListener('submit', (e) => {
 
 // Активируем скроллбары
 OverlayScrollbars(report, {});
+// Ф-я асинхронная, т.к. делает запросы к серверу
+const getData = async (url) => {
+    // Пишем конструкцию try-catch для защиты от ошибок
+    try {
+        // Получаем данные
+        const response = await fetch(`${API_URL}${url}`)
+        // Проверка на то, что данные не ок
+        if (!response.ok) {
+            // Создаем ошибку
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // Если все ок
+        return await response.json();
+    } catch (error) {
+        console.error('Ошибка при получении данных', error);
+        throw error;
+    }
+}
 
 // Ф-я закрывает отчет
 // В параметрах получаем target деструктуризацией
@@ -44,17 +66,52 @@ const closeReport = ({target}) => {
     // окно закрывается при нажатии на крестик или вне окна.
     if (target.closest('.report__close') || (!target.closest('.report') && target !== financeReport)) {
     report.classList.remove('report__open');
+    // Убираем событие с документа, что бы оно не висело и не остлеживало клики
+    document.removeEventListener('click', closeReport);
     }
 };
 
 // Ф-я открывает отчет
 const openReport = () => {
     report.classList.add('report__open');
-
+// Навешиваем событие на документ, которое закроет окно при клике на крестик или вмне отчета
 document.addEventListener('click', closeReport);
+};
+// Ф-я отрисовывает таблицу на основе данных
+const renderReport = (data) => {
+    reportOperationList.textContent = 'Загрузка...';
+
+    // Формируем таблицу (перебираем операции и строим строки в таблице)
+    const reportRows = data.map((operation) => {
+        const reportRow = document.createElement('tr');
+        reportRow.classList.add('report__row');
+
+        reportRow.innerHTML = `
+        <td class="report__cell">Продукты питания</td>
+        <td class="report__cell">1000</td>
+        <td class="report__cell">Магазин Spar</td>
+        <td class="report__cell">01.01.2021</td>
+        <td class="report__cell">расход</td>
+        <td class="report__action-cell">
+            <button
+            class="report__button report__button_table">&#10006;</button>
+        </td>
+        `;
+
+        return reportRow;
+    });
+    reportOperationList.append(...reportRows);
 };
 
 // Навешиваем событие на клик по кнопке "отчет"
-financeReport.addEventListener('click', openReport);
+financeReport.addEventListener('click', async () => {
+    openReport();
+    // Ф-я делает запрос к серверу
+    // getData - асинхронная ф-я, поэтому нужно дождаться данных (пишем await)
+    const data = await getData('/test');
+    console.log('data: ', data);
+    // После получения данных с сервера вызовем ф-ю 
+    renderReport(data);
+});
 
 
